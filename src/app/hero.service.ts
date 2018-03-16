8786d9f4e39fbc189963908ee80132b81b8d78af
Phillipe <phillrog@hotmail.com>
2018-03-16 03:58:49 -0300

    The Observable class of the RxJS library was used to return the array of heroes asynchronously.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './modelo/hero';
 import { HEROES } from './mock/mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
