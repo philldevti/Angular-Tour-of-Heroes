59f349e2f6159bec5678a4fdc011dfee0c09bbb9
Phillipe <phillrog@hotmail.com>
2018-03-17 23:41:50 -0300

    It's was created the route to access dashboard from the root of site.

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HeroesComponent } from '../heroes/heroes.component';
 import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent}
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
